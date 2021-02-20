abfefee921c98964f425b748214fb70e17315418
Ehsan Yousefzadeh <ehsanyusefzadehasl@gmail.com>
2021-02-20 06:07:16 +0330

    cleaned price extracted and also learned how to use group(1) to use the grouped result

--- code/06-final_project/final_project.py
+++ code/06-final_project/final_project.py
@@ -2,6 +2,15 @@ import requests
 from bs4 import BeautifulSoup
 import re
 
+def number_comma_cleaner(number):
+    temp_list = number.split(',')
+    # print(temp_list)
+    tmp = ""
+    for i in temp_list:
+        tmp = tmp + i
+    # print(tmp)
+    return int(tmp)
+
 number_of_pages_to_scrape = 2
 main_url = "https://bama.ir/car/all-brands/all-models/all-trims?page="
 
@@ -13,32 +22,62 @@ for i in range(1, number_of_pages_to_scrape + 1):
     # print("$$$$$$$$$$$$$$$$$$$$$$ ------------------page_number: ", i)
     # print(result.text)
 
-    soup = BeautifulSoup(result.text, 'html.parser')
+    page_soup = BeautifulSoup(result.text, 'html.parser')
 
     # print(soup)
 
-    res_links = soup.find_all('a', attrs={'class': 'cartitle'})
+    res_links = page_soup.find_all('a', attrs={'class': 'cartitle-desktop'})
     # This regular expression is for extracting links from which we are going to get our real data
     post_link_regular_expr = r'href="(.*?)"'
 
-    
     for e in res_links:
         result_links = re.search(post_link_regular_expr, str(e))
-        # print(result_links.group(0))
+        print(result_links.group(0))
         # print("===========")
         # Now, we have link to every post, so we can extract data from each of them
         # In each post, we have to check whether it has a price or not
         # If a post does not have price, it is useless for our model
 
         post_url = str(result_links.group(0))
-        print(post_url)    
+        # print(post_url[6:len(post_url) - 1])
 
         # Then, we go to scrape data from each post
+        post_url_to_req = post_url[6:len(post_url)-1]
+        post = requests.get(post_url_to_req)
+        post_soup = BeautifulSoup(post.text, 'html.parser')
+
         # brand, model, year, price, usage, gearbox, fuel, body_condition, color, province, motor_volume, 
         # number_of_cylinder, acceleration, fuel_usage
 
-        # We have to have different regular expressions forextracting these features
 
+        # First of all, checking the price, if not price, we will continue
+        # with the next post
+        post_price_check_regex = r'<p>\s*<span\sclass="label">قیمت\s*<\/span>\s*<span\scontent=".*>\sتومان'
+        post_price_check_result = re.search(
+            post_price_check_regex, str(post_soup))
+
+        if post_price_check_result == None:
+            # print("No Price tag")
+            # So, we will skip it
+            pass
+        else:
+            # print("it seems that this post has a price tag")
+            # So, first of all, we extract and clean the price
+            exact_price_regex = r'<span class="label">قیمت\s*<\/span>\s*<span\scontent="(.*?)"'
+            post_price_exact_result = re.search(
+                exact_price_regex, str(post_soup))
+
+            # We learned here how to use group(1) to get the specified group
+            # In the previous examples, we used group(0) which made our efforts useless
+            price_to_be_cleaned = post_price_exact_result.group(1)
+            cleaned_price = number_comma_cleaner(price_to_be_cleaned)
+            # Now, we have the price, we have to extract other information
+
+
+            # brand, model, year
+            
+        # We have to have different regular expressions forextracting these features
+        
 
 
         # Then, each of the above mentioned will go into our database
